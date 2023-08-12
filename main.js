d91b783683db4217c37bbf77418b2bd9a65a96aa
Tien <tienminh1351@gmail.com>
2023-08-12 16:49:51 -0700

    initialized session

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const signinRouter = require('./Routes/signin');
+const session = require("express-session");
+const store = new session.MemoryStore();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store,
+        cookie: { maxAge: 1000 * 60 * 60 * 24, secure: true, sameSite: "none" }
+    })
+)
 
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', 'https://tiend98.github.io');

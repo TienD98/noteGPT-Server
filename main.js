5b13e8d42d0299c3fbe4f967cd1217acf15bca8c
Tien <tienminh1351@gmail.com>
2023-08-13 19:25:47 -0700

    update

--- main.js
+++ main.js
@@ -22,21 +22,15 @@ app.use(
 )
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 
+const corsOptions = {
+    origin: ['https://tiend98.github.io', 'http://localhost:5173'],
+    credentials: true,
+    methods: 'GET, POST, PUT, DELETE',
+};
 
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://tiend98.github.io', 'http://localhost:5173'];
-    const origin = req.headers.origin;
-
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Hello, Wolrd!');

89c70d19cc43bc8bb5a4dd5493811782894fc6e2
Tien <tienminh1351@gmail.com>
2023-08-13 18:42:03 -0700

    test

--- main.js
+++ main.js
@@ -25,12 +25,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://tiend98.github.io');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://tiend98.github.io');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello, Wolrd!');

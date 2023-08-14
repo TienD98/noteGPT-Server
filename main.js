ccff1ebce06a909667174eead91c4d506f2e9423
Tien <tienminh1351@gmail.com>
2023-08-13 22:13:37 -0700

    update

--- main.js
+++ main.js
@@ -25,13 +25,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://tiend98.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true'),
         next();
 });
-
 app.set('trust proxy', 1);
 app.use(
     session({

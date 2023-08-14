6c8ef11d60142b384c0618dd93738c7db33921c6
Tien <tienminh1351@gmail.com>
2023-08-14 11:50:18 -0700

    update

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ const store = new session.MemoryStore();
 const cookieParser = require('cookie-parser');
 const corsOptions = {
     origin: ["https://tiend98.github.io", "http://localhost:3000"],
-    credentials: true
+    credentials: true,
+    allowedHeader: true
 };
 
 app.use(cookieParser());
@@ -59,56 +60,3 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 })
-// const express = require('express');
-// const session = require('express-session');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-// const app = express();
-
-// const corsOptions = {
-//     origin: "https://tiend98.github.io",
-//     credentials: true
-// };
-// app.use((req, res, next) => {
-//     res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     console.log('CORS Pre-Flight:', req.headers.origin);
-//     next();
-// }, cors(corsOptions));
-
-// app.use(cookieParser());
-// app.enable('trust proxy');
-// app.use(session({
-//     secret: 'your-secret-key',
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//         sameSite: 'none', // or 'strict', 'lax' as needed
-//         secure: true,
-//         httpOnly: false
-//     }
-// }));
-
-// app.get('/noteGPT', (req, res) => {
-//     // Check if the session has a visit count, if not, set it to 1
-//     req.session.visitCount = req.session.visitCount ? req.session.visitCount + 1 : 1;
-//     console.log(req.session.visitCount);
-//     console.log('Session:', req.session);
-
-//     res.send(`You have visited this page ${req.session.visitCount} times.`);
-// });
-
-// app.get('/setcookie', (req, res) => {
-//     res.cookie('test', 'value', { sameSite: 'none', secure: true });
-//     console.log('Session:', req.session);
-
-//     res.send('Cookie set');
-// });
-
-// const PORT = process.env.PORT || 5001;
-// app.listen(PORT, () => {
-//     console.log(`Server is listening on port ${PORT}`);
-// })

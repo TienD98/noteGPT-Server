719628f39a40d360785ad3969077b1dfc2ab5c12
Tien <tienminh1351@gmail.com>
2023-08-14 00:43:17 -0700

    update

--- Routes/welcome.js
+++ Routes/welcome.js
@@ -2,7 +2,16 @@ const express = require('express');
 const welcomeRouter = express.Router();
 const cors = require('cors');
 
-welcomeRouter.use(cors());
+// welcomeRouter.use(cors());
+welcomeRouter.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "http://localhost:5173",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+        "Access-Control-Allow-Credentials": "true"
+    });
+    next();
+});
 
 welcomeRouter.get('/', (req, res) => {
     res.status(200).json(req.session.user);

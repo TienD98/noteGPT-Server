919badf11c86470be2fe4a0fcbbfc1852a3b87b5
Tien <tienminh1351@gmail.com>
2023-08-13 23:45:56 -0700

    update

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 const corsOptions = {
-    origin: 'https://tiend98.github.io/noteGPT',
+    origin: 'https://tiend98.github.io',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true, // This allows cookies to be sent with the request
     optionsSuccessStatus: 204,

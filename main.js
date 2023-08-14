363ead66d0a2f5f4e3d847ef52195603cde51244
Tien <tienminh1351@gmail.com>
2023-08-14 10:22:33 -0700

    update

--- main.js
+++ main.js
@@ -71,7 +71,9 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://tiend98.github.io"
+}));
 app.use(cookieParser());
 
 app.use(session({

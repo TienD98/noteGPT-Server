69b5e9c6939ef4ce4846f3f748ac7c138f02d649
Tien <tienminh1351@gmail.com>
2023-08-14 09:56:14 -0700

    update

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ app.use(
         resave: false,
         saveUninitialized: true,
         store,
-        cookie: { SameSite: "None", httpOnly: false, maxAge: 1000 * 60 * 60 * 24, secure: true }
+        cookie: { sameSite: "None", httpOnly: false, maxAge: 1000 * 60 * 60 * 24, secure: true }
     })
 );
 

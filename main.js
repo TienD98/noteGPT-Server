0c3b44ca500be28906c842f1d4557c9c250b59ce
Tien <tienminh1351@gmail.com>
2023-08-14 10:09:34 -0700

    update

--- main.js
+++ main.js
@@ -83,6 +83,8 @@ app.get('/', (req, res) => {
     res.send(`You have visited this page ${req.session.visitCount} times.`);
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})

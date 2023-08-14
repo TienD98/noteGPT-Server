b7808c200a0b17e5a00d5d5929283ae1396a6dc3
Tien <tienminh1351@gmail.com>
2023-08-14 00:47:25 -0700

    up

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ const store = new session.MemoryStore();
 app.use((req, res, next) => {
     res.set({
         "Access-Control-Allow-Origin": "http://localhost:5173",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
         "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
     });
     next();

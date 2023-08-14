888a373a883cc27b129c61bb01ecf9e7462e1dea
Tien <tienminh1351@gmail.com>
2023-08-14 10:20:22 -0700

    test

--- main.js
+++ main.js
@@ -68,11 +68,11 @@
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
 
 app.use(cors());
 app.use(cookieParser());
-const app = express();
 
 app.use(session({
     secret: 'your-secret-key',

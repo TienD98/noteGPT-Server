6a579d6dada8b14c9d299a76237727f0cd6055cb
Tien <tienminh1351@gmail.com>
2023-08-16 13:54:06 -0700

    add route /profile to get user info

--- Routes/welcome.js
+++ Routes/welcome.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const pool = require('../DB/db');
 const welcomeRouter = express.Router();
 
 welcomeRouter.get('/', (req, res) => {
     res.status(200).send('welcome!');
+});
+
+welcomeRouter.get('/profile', (req, res) => {
+    pool.query('select * from users where id = $1', [req.session.passport.user])
+        .then((result) => {
+            return res.status(200).send(result.rows[0]);
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).send('database err');
+        })
 })
 
 module.exports = welcomeRouter

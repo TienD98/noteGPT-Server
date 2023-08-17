e5763487a5025a3015be962412e9310ec0dc2327
Tien <tienminh1351@gmail.com>
2023-08-17 12:16:48 -0700

    fix typo

--- Routes/validate.js
+++ Routes/validate.js
@@ -3,7 +3,7 @@ const validateRouter = express.Router();
 
 validateRouter.get('/', (req, res) => {
     if (req.isAuthenticated()) {
-        return res.send(200).json({ authenticate: true });
+        return res.status(200).json({ authenticate: true });
     }
     return res.status(401).send('not login');
 });

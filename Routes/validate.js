6d3869d6d97287992f95d93177bad95208584b96
Tien <tienminh1351@gmail.com>
2023-08-17 11:39:11 -0700

    validate route to check authenticate

---
+++ Routes/validate.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const validateRouter = express.Router();
+
+githubRouter.get('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.send(200).json({ authenticate: true });
+    }
+    return res.status(401).send('not login');
+});
+
+module.exports = validateRouter

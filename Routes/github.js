a794c7df8da868e2d7b617d20f748fe4f71c48e2
Tien <tienminh1351@gmail.com>
2023-08-16 20:16:40 -0700

    update oauth github

--- Routes/github.js
+++ Routes/github.js
@@ -4,26 +4,18 @@ const githubRouter = express.Router();
 
 githubRouter.get('/', passport.authenticate("github", { scope: ["user"] }));
 
-// githubRouter.get('/callback', (req, res, next) => {
-//     passport.authenticate('github', (err, user, info) => {
-//         if (err) { return res.status(500).send('error server') };
-//         if (!user) { return res.status(404).send('usernot found ' + info) };
-//         req.logIn(user, (err) => {
-//             if (err) {
-//                 return res.status(500).send(err);
-//             }
-//             // Redirect or respond as appropriate for your application
-//             return res.status(200).send('success');
-//         });
-//     })
-
-// });
-
-githubRouter.get('/callback',
-    passport.authenticate('github', { failureRedirect: '/login' }),
-    function (req, res) {
-        // Successful authentication
-        res.redirect('/');
-    });
+githubRouter.get('/callback', (req, res, next) => {
+    passport.authenticate('github', (err, user, info) => {
+        if (err) { return res.status(500).send('error server'); }
+        if (!user) { return res.status(404).send('user not found ' + info); }
+        req.logIn(user, (err) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            // Redirect or respond as appropriate for your application
+            return res.status(200).send('success');
+        });
+    })(req, res, next); // Make sure to call the function with req, res, and next
+});
 
 module.exports = githubRouter;

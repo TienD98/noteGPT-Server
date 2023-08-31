90344e876cf438b616e7d46012bd517515aae2ff
Tien <tienminh1351@gmail.com>
2023-08-31 16:32:52 -0700

    update test

--- Routes/github.js
+++ Routes/github.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const pool = require('../DB/db');
 const githubRouter = express.Router();
 
 githubRouter.get('/', passport.authenticate("github", { scope: ["user"] }));
@@ -7,8 +8,14 @@ githubRouter.get('/', passport.authenticate("github", { scope: ["user"] }));
 githubRouter.get('/callback',
     passport.authenticate('github',
         { failureRedirect: 'https://tiend98.github.io/noteGPT/#/login' }), (req, res) => {
-            res.status(200).send('hi');
             res.redirect('https://tiend98.github.io/noteGPT/#/main');
+            pool.query('INSERT INTO notes (users_id, title, note) values ($1, $2, $3);', [req.user.id, 'Welcome', 'Here is where you can write notes'], (error, results) => {
+                if (error) {
+                    console.log(error.message);
+                } else {
+                    console.log(results);
+                }
+            });
         });
 
 module.exports = githubRouter;
